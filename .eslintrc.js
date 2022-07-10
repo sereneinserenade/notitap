41bd1aba5c3ae1b53747b7a116ba3b838c5b6814
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2022-07-10 18:55:14 +0200

    feat: slash menu

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     "import/prefer-default-export": 0,
     "react/function-component-definition": 0,
     "import/no-extraneous-dependencies": 0,
+    "@typescript-eslint/no-use-before-define": ["warn"],
   },
   ignorePatterns: ["**/*.json", "**/vendor/*.js"],
 };
--- src/tiptap/extensions/config.ts
+++ src/tiptap/extensions/config.ts
@@ -1,3 +1,14 @@
+import Placeholder from "@tiptap/extension-placeholder";
+
+import { Commands, suggestions } from "../menus";
 import { CustomStarterKit } from "./starterKit";
 
-export const extensions = [CustomStarterKit];
+export const extensions = [
+  CustomStarterKit,
+  Commands.configure({
+    suggestions,
+  }),
+  Placeholder.configure({
+    placeholder: "Type '/' for commands",
+  }),
+];
